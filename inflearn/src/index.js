541f21025643f2ee7c5773d73925cb68d57c58c8
geosuk <geosuk9808@gmail.com>
2024-06-28 01:17:29 +0900

    get, post, patch, delete

--- inflearn/src/index.js
+++ inflearn/src/index.js
@@ -1,23 +1,63 @@
-const cors = require('cors');
-const helmet = require('helmet');
-const dayjs = require('dayjs');
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const app = express();
-app.use(cors());
-app.use(helmet());
 
-const today = new Date();
-const todayToDayjs = dayjs(today).format("YYYY-MM-DD");
-console.log(todayToDayjs);
+let users = [
+    {
+    id : 1, 
+    name : 'labil',
+    age: 12
+}
+]
 
-const password = '1234';
-const hashedPassword = bcrypt.hashSync(password, 10);
-console.log(hashedPassword);
+app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: "700mb"}));
 
-const token = jwt.sign('1234', 'dsfasf232werg');
-console.log({token});
+// 유저 정보 가져오기
+// 성공하게 되면 status : 200
+app.get("/users", (req, res)=>{
+    res.status(200).json({users});
+})
+
+// 유저 생성
+// 성공하게 되면 status : 201
+app.post('/users', (req, res)=>{
+    const {name, age} = req.body;
+    console.log('body: ', req.body);
+
+    users.push({
+        id: new Date().getTime(), 
+        name : 'labil2',
+        age,
+    })
+    res.status(201).json({users});
+})
+
+// 유저 수정
+// 성공하게 되면 status : 204
+// req.params.id
+app.patch('/users/:id', (req, res)=>{
+    const { id } = req.params;
+    const {name, age} = req.body;
+    console.log('param', req.params);
+    const targetUserIdx = users.findIndex((user)=> user.id === Number(id))
+    users[targetUserIdx] = {
+        id : users[targetUserIdx].id,
+        name: name ? name : users[targetUserIdx].name,
+        age: age ?? users[targetUserIdx].age
+    };
+    res.status(204).json();
+})
+
+// 유저 삭제
+// 성공하게 되면 status : 204
+app.delete('/users/:id', (req, rew)=>{
+    const {id} = req.params;
+
+    const deletedUsers = users.filter((user)=>user.id !== Number(id));
+    users = deletedUsers;
+
+    res.status(204).json();
+})
 
 app.use('/', (req, res)=>{
     res.send('서버 실행중');
