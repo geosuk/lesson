f0bb4169c30ab59c8bdd20727f26af45379dd4d1
geosuk <geosuk9808@gmail.com>
2024-06-27 23:43:37 +0900

    연습

--- inflearn/src/index.js
+++ inflearn/src/index.js
@@ -2,12 +2,22 @@ const cors = require('cors');
 const helmet = require('helmet');
 const dayjs = require('dayjs');
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(cors());
 app.use(helmet());
 
 const today = new Date();
-const todayToDayjs = dayjs(today).formet("YYYY-<<-DD");
+const todayToDayjs = dayjs(today).format("YYYY-MM-DD");
+console.log(todayToDayjs);
+
+const password = '1234';
+const hashedPassword = bcrypt.hashSync(password, 10);
+console.log(hashedPassword);
+
+const token = jwt.sign('1234', 'dsfasf232werg');
+console.log({token});
 
 app.use('/', (req, res)=>{
     res.send('서버 실행중');
